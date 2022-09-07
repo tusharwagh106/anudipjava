51ec1b919605a19d4f3a7864e168d73d9ee76fba
tusharwagh106 <113004424+tusharwagh106@users.noreply.github.com>
2022-09-07 12:23:10 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,15 @@
+class IfElse
+{
+public static void main(String args[])
+{
+char ch='i';
+if(ch=='i')
+{
+System.out.println("i is vowel");
+}
+else
+{
+System.out.println("i is consonent");
+}
+}
+}
